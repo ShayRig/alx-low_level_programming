723a72dd4b7aa6c3f2303f79b1940e0a663148fc
ShayRig <arishakech@gmail.com>
2022-09-08 14:57:18 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
- * main - Points a text
+ * Description: main - Prints out a string to stdout
  *
- * Return: Always 1 (Success)
+ * Return: 1 if success
  */
+int main(void)
 { 
-	write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
+	write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
 	return (1);
 }
 
