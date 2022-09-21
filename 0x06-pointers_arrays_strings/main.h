ed8f56edc1199beee81e482cb39d4797ece1a62d
ShayRig <arishakech@gmail.com>
2022-09-21 14:06:12 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,8 +9,8 @@
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
-int_strcmp(char *b, int size);
-char *_strcat(char *s1, char *s2);
+int_strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
@@ -20,4 +20,4 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif/*MAIN_H*/
+#endif /*MAIN_H*/
