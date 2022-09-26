6e7b35591c08cc1d9cec3fa96aa36758d6aa7bb0
ShayRig <arishakech@gmail.com>
2022-09-26 14:54:50 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
- * void prototypes()
- * int prototypes()
- */
+* void prototypes()
+* int prototypes()
+*/
 
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
@@ -14,6 +14,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to)
+void set_string(char **s, char *to);
 
 #endif /*MAIN_H*/
