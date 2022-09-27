9e39525622ed74ae58cdd23a68f447f0deb8d1cd
ShayRig <arishakech@gmail.com>
2022-09-27 06:49:36 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: string to check
  * @c: character to check for
  *
- * Return: pointer to spot in s with c or null
+ * Return: pointer to spot in s with c or NULL
  */
 char *_strchr(char *s, char c);
 {
