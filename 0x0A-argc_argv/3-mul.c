846c137a2590581f748e64df38b8f0a64fcac150
ShayRig <arishakech@gmail.com>
2022-09-30 11:38:34 -0700

    main.h

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,12 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main -> this is a function to print its name
+ * main - this is a function to print its name
+ *
  * @argc: argc parameter
  * @argv: an array of a command listed
+ *
  * Return : 0 for success
  */
 int main(int argc, char argv[])
