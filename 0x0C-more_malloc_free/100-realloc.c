f91c9704e3553aa5dbb2d844e95b1e772a1ab90c
ShayRig <arishakech@gmail.com>
2022-10-06 03:40:30 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,12 +29,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	p = malloc(new_size);
-	if (p ==NULL)
+	if (p == NULL)
 		return (NULL);
 	if (new_size > old_size)
 		max = old_size;
 	for (i = 0; i < max; i++)
 		p[i] = oldp[i];
-	free ptr;
+	free(ptr);
 	return (p);
 }
