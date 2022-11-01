1a1ae55eb7ac3ec8df81db4dc396db2edc9baaea
ShayRig <arishakech@gmail.com>
2022-11-01 11:11:04 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - returns the value of a bit at a given index.
- * n: number to check bits in
+ * @n: number to check bits in
  * @index: index at which to check bit
  *
  * Return: value of the bit, or -1 if there is an error
